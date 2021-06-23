cb98fb6d7c9cb7a60e05250f4b52ee77a3af0556
Parmeet Singh <76438148+singhmeet11@users.noreply.github.com>
2021-06-23 21:54:23 +0530

    fixxed issue 6397 and added a testing file for matplot lib images

---
+++ test/ipynb/mpl/test_graph_matplotlib_drawer.py
@@ -0,0 +1,96 @@
+# This code is part of Qiskit.
+#
+# (C) Copyright IBM 2020.
+#
+# This code is licensed under the Apache License, Version 2.0. You may
+# obtain a copy of this license in the LICENSE.txt file in the root directory
+# of this source tree or at http://www.apache.org/licenses/LICENSE-2.0.
+#
+# Any modifications or derivative works of this code must retain this
+# copyright notice, and modified files need to carry a notice indicating
+# that they have been altered from the originals.
+
+""" Tests for circuit MPL drawer"""
+
+import unittest
+
+import json
+import os
+from contextlib import contextmanager
+import math
+import numpy as np
+from numpy import pi
+
+from qiskit.visualization.state_visualization import state_drawer
+from qiskit import BasicAer, execute 
+from qiskit.test import QiskitTestCase
+from qiskit import QuantumCircuit, QuantumRegister, ClassicalRegister, transpile
+from qiskit.test.mock import FakeTenerife
+from qiskit.circuit.library import XGate, MCXGate, HGate, RZZGate, SwapGate, DCXGate, ZGate
+from qiskit.circuit.library import MCXVChain
+from qiskit.extensions import HamiltonianGate
+from qiskit.circuit import Parameter
+from qiskit.circuit.library import IQP
+from qiskit.quantum_info.random import random_unitary
+from qiskit.tools.visualization import HAS_MATPLOTLIB
+
+if HAS_MATPLOTLIB:
+    from matplotlib.pyplot import close as mpl_close
+else:
+    raise ImportError(
+        "Must have Matplotlib installed. To install, run " '"pip install matplotlib".'
+    )
+
+
+RESULTDIR = os.path.dirname(os.path.abspath(__file__))
+
+
+@contextmanager
+def cwd(path):
+    """A context manager to run in a particular path"""
+    oldpwd = os.getcwd()
+    os.chdir(path)
+    try:
+        yield
+    finally:
+        os.chdir(oldpwd)
+
+
+class TestGraphMatplotlibDrawer(QiskitTestCase):
+    """Graph MPL visualization"""
+
+    def setUp(self):
+        super().setUp()
+        self.graph_drawer = TestGraphMatplotlibDrawer.save_data_wrap(
+            state_drawer, str(self)
+        )
+
+    def tearDown(self):
+        super().tearDown()
+        mpl_close("all")
+
+    @staticmethod
+    def save_data_wrap(func, testname):
+        """A wrapper to save the data from a test"""
+
+        def wrapper(*args, **kwargs):
+            image_filename = kwargs["filename"]
+            with cwd(RESULTDIR):
+                results = func(*args, **kwargs)
+                TestMatplotlibDrawer.save_data(image_filename, testname)
+            return results
+
+        return wrapper
+
+    @staticmethod
+    def save_data(image_filename, testname):
+        """Saves result data of a test"""
+        datafilename = "result_test.json"
+        if os.path.exists(datafilename):
+            with open(datafilename) as datafile:
+                data = json.load(datafile)
+        else:
+            data = {}
+        data[image_filename] = {"testname": testname}
+        with open(datafilename, "w") as datafile:
+            json.dump(data, datafile)
